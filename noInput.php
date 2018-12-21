a851fe66027d42d86dae6c4f962f33d15b6e1630
YangCao1708 <samcy1708@gmail.com>
2018-12-20 21:14:32 -0500

    noinput.php and typeLocation.php

---
+++ noInput.php
@@ -0,0 +1,143 @@
+<head>
+	<title>Top Crash Types In Selected City</title>
+</head>
+
+<body>
+
+	<?php
+		$dbhost = 'dbase.cs.jhu.edu:3306';
+		$dbuser = 'ycao29';
+		$dbpass = 'wyxjaycqli';
+		$dbname = 'cs41518_ycao29_db';
+		$db = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+
+		if(mysqli_connect_errno()){
+			echo mysqli_connect_error();
+		}
+		
+		echo "Part 1: which type of crash (pedestrian or bike) has higher severity of injury?\n";
+		echo "Pedestrian crash:\n";
+		$result = $db->query("CALL Injury_Ped");
+		if (!$result){
+			echo "Fail to retrieve info for pedestrian crashes!\n";
+			print mysql_error();
+		} else {
+			echo "<table border=1>\n";
+			echo "<tr><td>Crash Severity Level</td><td>Percentage</td></tr>\n";
+			while ($row = mysqli_fetch_array($result)) {
+				$crsh_sevri = $row['crsh_sevri'];
+			    $percentage = $row['percentage'];
+			    echo "<tr><td>".$crsh_sevri."</td><td>".$percentage."</td>></tr>";
+			}
+			echo "</table>\n";
+			$result->close();
+		    $db->next_result();
+		}
+
+		echo "Bike crash:\n";
+		$result = $db->query("CALL Injury_Bike");
+		if (!$result){
+			echo "Fail to retrieve info for bike crashes!\n";
+			print mysql_error();
+		} else {
+			echo "<table border=1>\n";
+			echo "<tr><td>Crash Severity Level</td><td>Percentage</td></tr>\n";
+			while ($row = mysqli_fetch_array($result)) {
+				$crsh_sevri = $row['crsh_sevri'];
+			    $percentage = $row['percentage'];
+			    echo "<tr><td>".$crsh_sevri."</td><td>".$percentage."</td>></tr>";
+			}
+			echo "</table>\n";
+			$result->close();
+		    $db->next_result();
+		}
+
+		echo "A list of all injury types:\n";
+		$result = $db->query("CALL Injury_Bike");
+		if (!$result){
+			echo "Fail to retrieve info!\n";
+			print mysql_error();
+		} else {
+			echo "<table border=1>\n";
+			echo "<tr><td>Crash Severity Level</td></tr>\n";
+			while ($row = mysqli_fetch_array($result)) {
+				$crsh_sevri = $row['crsh_sevri'];
+			    echo "<tr><td>".$crsh_sevri."</td></tr>";
+			}
+			echo "</table>\n";
+			$result->close();
+		    $db->next_result();
+		}
+
+
+		echo "Part 2: for both pedestrian and bike crashes, do crashes happen more often at intersection/non-intersection?\n";
+		$result = $db->query("CALL IntersectAccidentRate");
+		if (!$result){
+			echo "Fail to retrieve info!\n";
+			print mysql_error();
+		} else {
+			echo "<table border=1>\n";
+			echo "<tr><td>Crash Location</td><td>Percentage</td><td>Most Frequent Severity</td><td>Most Frequent Weather</td><td>Most Frequent Light Condition</td><td>Most Frequent # of Lanes</td></tr>\n";
+			while ($row = mysqli_fetch_array($result)) {
+				$crsh_loc = $row['crsh_loc'];
+			    $percentage = $row['percentage'];
+			    $crsh_sevri = $row['crsh_sevri'];
+			    $weather = $row['weather'];
+			    $light_cond = $row['light_cond'];
+			    $num_lanes = $row['num_lanes'];
+			    echo "<tr><td>".$crsh_loc."</td><td>".$percentage."</td>><td>".$crsh_sevri."</td><td>".$weather."</td><td>".$light_cond."</td><td>".$num_lanes."</td></tr>";
+			}
+			echo "</table>\n";
+			$result->close();
+		    $db->next_result();
+		}
+
+		echo "Part 3: do crashes happen more often when there is no traffic control?\n";
+		echo "Pedestrian crash:\n";
+		$result = $db->query("CALL Traffic_Ped");
+		if (!$result){
+			echo "Fail to retrieve info for pedestrian crashes!\n";
+			print mysql_error();
+		} else {
+			echo "<table border=1>\n";
+			echo "<tr><td>Traffic Control</td><td>Percentage</td><td>Most Frequent Severity</td><td>Most Frequent Weather</td><td>Most Frequent Light Condition</td><td>Most Frequent # of Lanes</td></tr>\n";
+			while ($row = mysqli_fetch_array($result)) {
+				$traff_cntr = $row['traff_cntr'];
+			    $TrafficControlRate = $row['TrafficControlRate'];
+			    $crsh_sevri = $row['crsh_sevri'];
+			    $weather = $row['weather'];
+			    $light_cond = $row['light_cond'];
+			    $num_lanes = $row['num_lanes'];
+			    echo "<tr><td>".$traff_cntr."</td><td>".$TrafficControlRate."</td>><td>".$crsh_sevri."</td>><td>".$weather."</td>><td>".$light_cond."</td>><td>".$num_lanes."</td>></tr>";
+			}
+			echo "</table>\n";
+			$result->close();
+		    $db->next_result();
+		}
+
+		echo "Bike crash:\n";
+		$result = $db->query("CALL Traffic_Bike");
+		if (!$result){
+			echo "Fail to retrieve info for bike crashes!\n";
+			print mysql_error();
+		} else {
+			echo "<table border=1>\n";
+			echo "<tr><td>Traffic Control</td><td>Percentage</td><td>Most Frequent Severity</td><td>Most Frequent Weather</td><td>Most Frequent Light Condition</td><td>Most Frequent # of Lanes</td></tr>\n";
+			while ($row = mysqli_fetch_array($result)) {
+				$traff_cntr = $row['traff_cntr'];
+			    $TrafficControlRate = $row['TrafficControlRate'];
+			    $crsh_sevri = $row['crsh_sevri'];
+			    $weather = $row['weather'];
+			    $light_cond = $row['light_cond'];
+			    $num_lanes = $row['num_lanes'];
+			    echo "<tr><td>".$traff_cntr."</td><td>".$TrafficControlRate."</td>><td>".$crsh_sevri."</td>><td>".$weather."</td>><td>".$light_cond."</td>><td>".$num_lanes."</td>></tr>";
+			}
+			echo "</table>\n";
+			$result->close();
+		    $db->next_result();
+		}
+
+		$db->close();
+	?>
+
+</body>
---
+++ typeLocation.php
@@ -0,0 +1,65 @@
+<head>
+	<title>Top Crash Types In Selected City</title>
+</head>
+
+<body>
+
+	<?php
+		$dbhost = 'dbase.cs.jhu.edu:3306';
+		$dbuser = 'ycao29';
+		$dbpass = 'wyxjaycqli';
+		$dbname = 'cs41518_ycao29_db';
+		$db = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+
+		if(mysqli_connect_errno()){
+			echo mysqli_connect_error();
+		}
+		
+		$location = $_POST['location'];
+
+		echo "Pedestrian crash:\n";
+		$result = $db->query("CALL LocAccidentRate_Ped('$location')");
+		if (!$result){
+			echo "Fail to retrieve info for pedestrian crashes!\n";
+			print mysql_error();
+		} else {
+			echo "<table border=1>\n";
+			echo "<tr><td>Percentage</td><td>Hit&Run</td><td>Driver Drink Alcohol</td><td>Most Frequent Weather</td><td>Most Frequent Severity</td></tr>\n";
+			while ($row = mysqli_fetch_array($result)) {
+			    $percentage = $row['percentage'];
+			    $hit_run = $row['hit_run'];
+			    $drvr_alc_d = $row['drvr_alc_d'];
+			    $weather = $row['weather'];
+			    $crsh_sevri = $row['crsh_sevri'];
+			    echo "<tr><td>".$percentage."</td><td>".$hit_run."</td><td>".$drvr_alc_d."</td><td>".$weather."</td><td>".$crsh_sevri."</td></tr>";
+			}
+			echo "</table>\n";
+			$result->close();
+		    $db->next_result();
+		}
+
+		echo "Bike crash:\n";
+		$result = $db->query("CALL LocAccidentRate_Bike('$location')");
+		if (!$result){
+			echo "Fail to retrieve info for bike crashes!\n";
+			print mysql_error();
+		} else {
+			echo "<table border=1>\n";
+			echo "<tr><td>Percentage</td><td>Hit&Run</td><td>Driver Drink Alcohol</td><td>Most Frequent Weather</td><td>Most Frequent Severity</td></tr>\n";
+			while ($row = mysqli_fetch_array($result)) {
+			    $percentage = $row['percentage'];
+			    $hit_run = $row['hit_run'];
+			    $drvr_alc_d = $row['drvr_alc_d'];
+			    $weather = $row['weather'];
+			    $crsh_sevri = $row['crsh_sevri'];
+			    echo "<tr><td>".$percentage."</td><td>".$hit_run."</td><td>".$drvr_alc_d."</td><td>".$weather."</td><td>".$crsh_sevri."</td></tr>";
+			}
+			echo "</table>\n";
+			$result->close();
+		    $db->next_result();
+		}
+
+		$db->close();
+	?>
+
+</body>
