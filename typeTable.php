088701247010c6c9418840121270b1eeab02be36
YangCao1708 <samcy1708@gmail.com>
2018-12-20 21:18:26 -0500

    typeTable.php ongoing

--- typeTable.php
+++ typeTable.php
@@ -15,7 +15,34 @@
 			echo mysqli_connect_error();
 		}
 		
-		$location = $_POST['typeLoc'];
+		$type = $_POST['typeTable'];
+
+		if ($type==="pedestrian") {
+			echo "Pedestrian crash:\n";
+			$result = $db->query("CALL LocAccidentRate_Ped('$location')");
+			if (!$result){
+				echo "Fail to retrieve info for pedestrian crashes!\n";
+				print mysql_error();
+			} else {
+				echo "<table border=1>\n";
+				echo "<tr><td>Percentage</td><td>Hit&Run</td><td>Driver Drink Alcohol</td><td>Most Frequent Weather</td><td>Most Frequent Severity</td></tr>\n";
+				while ($row = mysqli_fetch_array($result)) {
+			    	$percentage = $row['percentage'];
+			    	$hit_run = $row['hit_run'];
+			    	$drvr_alc_d = $row['drvr_alc_d'];
+			    	$weather = $row['weather'];
+			    	$crsh_sevri = $row['crsh_sevri'];
+			    	echo "<tr><td>".$percentage."</td><td>".$hit_run."</td><td>".$drvr_alc_d."</td><td>".$weather."</td><td>".$crsh_sevri."</td></tr>";
+				}
+				echo "</table>\n";
+				$result->close();
+		    	$db->next_result();
+			}
+		} elseif ($type==="bike") {
+
+		}
+
+
 
 		echo "Pedestrian crash:\n";
 		$result = $db->query("CALL LocAccidentRate_Ped('$location')");
